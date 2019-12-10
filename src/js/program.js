398afe1c87e095766810155fe4097d3c42d9b922
dsmtan <denisetan@live.nl>
2019-12-10 23:53:32 +0100

    added dynamic session cards

--- src/js/program.js
+++ src/js/program.js
@@ -1,25 +1,37 @@
 'use strict';
 
+let programGrid = document.querySelector('.program--grid');
+let speakers = [];
 let programSettings = [];
 let eventSessions = [];
 
 //templates
 const daysButtonTemplate = document.querySelector('#daysButtonTemplate')
   .content;
+const stageColumnTemplate = document.querySelector('#stageColumnTemplate')
+  .content;
+const sessionCardTemplate = document.querySelector('#sessionCardTemplate')
+  .content;
 
 window.onload = function() {
   //get all programSettings from JSON
   fetch('/src/js/programSettings.json')
     .then(res => res.json())
-    .then(programSettings => {
-      programSettings = programSettings;
+    .then(programData => {
+      programSettings = programData;
       fetchEventSessions(programSettings);
+    });
+
+  fetch('/src/js/speakers.json')
+    .then(res => res.json())
+    .then(speakerData => {
+      speakers = speakerData;
     });
 };
 
-const fetchEventSessions = ({ eventID, eventDays }) => {
+const fetchEventSessions = ({ eventID, eventDays, eventStages }) => {
   // get the right program sessions for this event
-  fetch('/src/js/programSessions.json')
+  fetch('/src/js/programSessions2.json')
     .then(res => res.json())
     .then(programSessions => {
       const eventProgram = programSessions.find(
@@ -27,8 +39,11 @@ const fetchEventSessions = ({ eventID, eventDays }) => {
       );
       eventSessions = eventProgram.eventSessions;
       console.log(eventSessions);
+      if (eventSessions.length > 0) {
+        constructDaysBar(eventDays);
+        constructStageColumns(eventDays, eventStages);
+      }
     });
-  constructDaysBar(eventDays);
 };
 
 const constructDaysBar = eventDays => {
@@ -49,10 +64,9 @@ const constructDaysBar = eventDays => {
       dayDateNumber,
       dayOfWeek,
     };
-    console.log(dayData);
     createDaysButton(dayData);
-    constructTimeline(dayStart, dayEnd, dayDurationHours);
   });
+  constructTimeline();
 };
 
 const daysBar = document.querySelector('.navbar--days');
@@ -74,9 +88,80 @@ const createDaysButton = dayData => {
 
 const constructTimeline = (dayStart, dayEnd, dayDurationHours) => {};
 
-const dayOnclickHandler = () => {
-  // get class of current on targetdiv
-  // fetch right data
+const constructStageColumns = (eventDays, eventStages) => {
+  const columnNumber = eventStages.length;
+  // set sass variable of column numbers in grid
+  if (eventStages.length > 0) {
+    document.documentElement.style.setProperty('--colNum', columnNumber);
+  }
+
+  //grab template
+  eventStages.map(stage => {
+    const stageColumnCopy = stageColumnTemplate.cloneNode(true);
+    let columnDiv = stageColumnCopy.querySelector('.column--stage');
+    columnDiv.id = 'column' + stage.replace(/\s/g, '');
+    stageColumnCopy.querySelector('.p--stagetitle').textContent = stage;
+    programGrid.appendChild(stageColumnCopy);
+
+    //for each stage display sessions
+  });
+  prepareColumnData(eventDays, eventStages);
+};
+
+const prepareColumnData = (eventDays, eventStages) => {
+  const sessionsFirstDay = eventSessions.filter(
+    session => session.sessionDate === eventDays[0].date
+  );
+  console.log(sessionsFirstDay);
+
+  eventStages.map(stage => {
+    const sessionsByStage = sessionsFirstDay.filter(
+      session => session.sessionStage === stage
+    );
+    console.log(sessionsByStage);
+    // for each stage append session items
+    displaySessionsByStage(stage, sessionsByStage);
+  });
+};
+
+const displaySessionsByStage = (stage, sessionsByStage) => {
+  sessionsByStage.map(session => {
+    const sessionCardCopy = sessionCardTemplate.cloneNode(true);
+    let sessionItem = sessionCardCopy.querySelector('.session--item');
+    sessionItem.id = session.sessionID;
+
+    // add session content
+    sessionCardCopy.querySelector('.session--title').textContent =
+      session.sessionTitle;
+    sessionCardCopy.querySelector('.session--description').textContent =
+      session.sessionDescription;
+
+    // if there are speakers - for each speaker create avatar
+    session.sessionSpeakers.length > 0 &&
+      session.sessionSpeakers.map(sessionSpeaker => {
+        console.log(sessionSpeaker);
+        const speakerImg = createSpeakerAvatar(sessionSpeaker);
+        const speakerDiv = sessionCardCopy.querySelector('.session--speakers');
+        speakerDiv.appendChild(speakerImg);
+      });
+
+    // for each session tag create tag
+
+    const parentID = '#column' + session.sessionStage.replace(/\s/g, '');
+    const parentColumn = document.querySelector(parentID);
+
+    parentColumn.appendChild(sessionCardCopy);
+  });
+};
+
+const createSpeakerAvatar = sessionSpeaker => {
+  const speakerData = speakers.find(
+    speakerObj => speakerObj.speakerName === sessionSpeaker
+  );
+  const speakerAvatar = document.createElement('img');
+  speakerAvatar.src = speakerData.speakerImgURL;
+  speakerAvatar.classList.add('session--avatar');
+  return speakerAvatar;
 };
 
 // render fixed timeline column in grid
