ac066f32234d3b6993a4aeb77d651e8cd00c3a30
dsmtan <denisetan@live.nl>
2019-12-19 20:59:13 +0100

    fixed responsive desktop styling

--- dist/json/programSettings.bundle.js
+++ dist/json/programSettings.bundle.js
@@ -1,1 +1,14 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{8:function(e){e.exports={eventID:"8760123",eventMainColor:"#945FE0",eventDays:[{date:"01-17-2020",startTime:"10:00",endTime:"18:00"},{date:"01-18-2020",startTime:"10:00",endTime:"18:00"},{date:"01-19-2020",startTime:"10:00",endTime:"18:00"}],eventStages:["Main Stage","Inspiration Stage","Panel Stage","Exhibition Space"],eventTags:["UX design","Product design","Marketing","Graphic design","Social media","Development","Business"],eventSessionTypes:[{name:"Networking",color:"#7C7AEE"},{name:"Workshop",color:"#F5C9C9"},{name:"Talk",color:"#AAE0EC"},{name:"Panel",color:"#f8d3a4"},{name:"Break",color:"#c4c4c4"}],eventTimeSlots:["Morning","Afternoon","Evening"]}}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["json/programSettings"],{
+
+/***/ "./src/js/json/programSettings.json":
+/*!******************************************!*\
+  !*** ./src/js/json/programSettings.json ***!
+  \******************************************/
+/*! exports provided: eventID, eventMainColor, eventDays, eventStages, eventTags, eventSessionTypes, eventTimeSlots, default */
+/***/ (function(module) {
+
+eval("module.exports = {\"eventID\":\"8760123\",\"eventMainColor\":\"#945FE0\",\"eventDays\":[{\"date\":\"01-17-2020\",\"startTime\":\"10:00\",\"endTime\":\"18:00\"},{\"date\":\"01-18-2020\",\"startTime\":\"10:00\",\"endTime\":\"18:00\"},{\"date\":\"01-19-2020\",\"startTime\":\"10:00\",\"endTime\":\"18:00\"}],\"eventStages\":[\"Main Stage\",\"Inspiration Stage\",\"Panel Stage\",\"Exhibition Space\"],\"eventTags\":[\"UX design\",\"Product design\",\"Marketing\",\"Graphic design\",\"Social media\",\"Development\",\"Business\"],\"eventSessionTypes\":[{\"name\":\"Networking\",\"color\":\"#7C7AEE\"},{\"name\":\"Workshop\",\"color\":\"#F5C9C9\"},{\"name\":\"Talk\",\"color\":\"#AAE0EC\"},{\"name\":\"Panel\",\"color\":\"#f8d3a4\"},{\"name\":\"Break\",\"color\":\"#c4c4c4\"}],\"eventTimeSlots\":[\"Morning\",\"Afternoon\",\"Evening\"]};\n\n//# sourceURL=webpack:///./src/js/json/programSettings.json?");
+
+/***/ })
+
+}]);
